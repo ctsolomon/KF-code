f15bc2c0c2eef8d43a41175706a103552ebf6d9c
Chris Solomon <chris.solomon@mcgill.ca>
2015-02-12 20:07:43 -0500

    kalmanState: allow missing obs; return covariance matrices of state estimates
    Two major changes in this commit:
    
     - Changed kalmanState in a manner similar to changes to kalmanStep to deal correctly with time points where some but not all state observations are missing
    
     - Save the covariance matrix for the state estimate at each time step, and return an array containing all of these matrices across the complete time series

--- kalmanState.R
+++ kalmanState.R
@@ -11,6 +11,7 @@
 #  drivers - A data.frame with the driver data needed for the ut matrix.
 
 kalmanState<-function(p,Y,H,drivers){
+  require(abind)
   with(drivers,{
     #Unpack estimated parameters
     r <- p[1]
@@ -18,16 +19,22 @@ kalmanState<-function(p,Y,H,drivers){
     k <- p[3]
     Q <- matrix(c(exp(p[4]),0,0,exp(p[5])),nrow=2,byrow=TRUE)	#process error covariance matrix [2x2]
     
-    #Initialize P from Q
+    #Initialize P (covariance matrix for estimate of state) from Q
     P <- Q
+    #Initialize POut to hold P estimate at each time point
+    POut <- P
       
     #Number of observations
     nObs <- nrow(Y)
     
+    #Number of state variables
+    nVar <- 2
+    
     #Initialize a (estimate of state)
     a <- matrix(as.numeric(Y[1,]),nrow=2,byrow=T)
+    #Initialize aOut to hold a estimate at each time point
     aOut <- a
-        
+    
     #Define matrix C, parameters of covariates [2x5]
     C=matrix(c(k,1,1,-iota,0,0,0,0,0,0,1,1),nrow=2,byrow=TRUE)
     
@@ -43,27 +50,39 @@ kalmanState<-function(p,Y,H,drivers){
       #New observation
       y=matrix(as.numeric(Y[t,]),nrow=2,byrow=TRUE)
       
-      #Predictions
+      #Predictions (a is estimate of state, P is covariance matrix of estimation error)
       a = (B %*% a) + (C%*%ut)
       P = (B %*% P %*% t(B))+Q
       
-      #Update (as long as there are no NA in the y vector at this time point)
-      if (any(is.na(y))==FALSE) {
-        V = y-a  # residuals
+      #Update
+      if (all(is.na(y))==FALSE) {  #If no obs at this time step, skip updating and don't accumulate to likelihood. Otherwise...
+        
+        #Identify state variables for which obs are available at this time step, adjust y etc for missing obs
+        #See Harvey Eq. 3.4.75 f.f. for this approach (weights matrix W etc)
+        avail <- which(is.na(y)==FALSE)
+        nT <- length(avail)
+        W <- matrix(0,nrow=nT,ncol=nVar,byrow=TRUE) 
+        W[cbind((1:length(avail)),avail)] <- 1
+        y[which(is.na(y))] <- 0 #To make matrix math in next line work
+        yT <- W %*% y
+        
+        #Update
+        V = yT-a[avail]  # residuals
         F = P+H
-        Finv_P = solve(F,P)
-        Finv_V = solve(F,V)
-        a = a + (P %*% Finv_V)
-        P = P - (P %*% Finv_P)
+        Finv_P = solve(F[avail,avail],P[avail,avail])
+        Finv_V = solve(F[avail,avail],V)
+        a[avail,] = a[avail,] + (P[avail,avail] %*% Finv_V)
+        P[avail,avail] = P[avail,avail] - (P[avail,avail] %*% Finv_P)
       }
       
-      #Save the estimate of a from this time step
+      #Save the estimates of a and from this time step
       aOut <- cbind(aOut,a)
+      POut <- abind(POut,P,along=3)
       
     } # End iteration
     
-    #Return the state estimates
-    return(aOut)
+    #Return the state estimates and their covariance matrices
+    return(list(a=aOut,P=POut))
     
   }) #end 'with'
 } #end function
